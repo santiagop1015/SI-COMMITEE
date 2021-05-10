5896791fa5f87c4d48eb9b700835f55df6f8f5b1
Santiagop <santiagop15@gmail.com>
2021-05-09 23:20:08 -0500

    coordinador: Registro y actualizacion de fechas

---
+++ comite/coordinadores/pages/16.6-registro_fecha.php
@@ -0,0 +1,26 @@
+<?php
+    date_default_timezone_set ('America/Bogota');
+	utf8_decode(extract($_POST));
+	$fecha=$_POST['fecha'];
+    /*if(isset($_POST['id'])) {
+      $id=$_POST['id'];
+    }*/
+    $action=$_POST['action'];
+    $programa=$_POST['programa'];
+
+
+	require("../../connect_db.php");
+
+
+    if($action == 'update') {
+        mysqli_query($mysqli,"UPDATE fechascom SET fechas='$fecha' WHERE programa='$programa'");
+        //$sentencia = "update fechascom set fechas=".$fecha." where id_user='$id_user' ";
+    } else {
+        mysqli_query($mysqli,"INSERT INTO fechascom VALUES('','$fecha','$programa')");
+    }
+
+				
+
+
+echo "1";
+//echo "update fechascom set fechas=".$fecha." where programa=".$programa;
